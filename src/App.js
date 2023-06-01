d1f354d41ad208d89c44fbde899e8bedcf6ba36b
sagar442-debug <bnradlyp1@gmail.com>
2023-06-01 22:35:11 +0545

    Making a normal todo list without using any local storage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todo, setTodo] = useState('')
+  const [todoList, setTodoList] = useState([])
+
+  const handleChange = (e) => {
+    setTodo(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    setTodoList([...todoList, todo])
+  }
+
+  const handleDelete=(index)=>{
+    const filteredData = todoList.filter((_,i)=>i!== index)
+    setTodoList(filteredData)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="input">
+        <input value={todo} onChange={handleChange} type="text" />
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+      <div>
+        {
+          todoList.map((todo,index) => {
+            return(
+              <ul key={index}>
+              <li>{todo} 
+                <button onClick={()=>handleDelete(index)}>X</button>
+              </li>
+            </ul>
+            )
+            
+          })
+        }
+      </div>
+
+
+
     </div>
   );
 }
